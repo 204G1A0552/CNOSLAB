c782151429fccd1084cdead099eb9e70af3528ae
204G1A0552 <94515230+204G1A0552@users.noreply.github.com>
2022-09-06 12:51:57 +0530

    P.Manjusha created PP.c

---
+++ OSLAB/PP.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: P.manjusha
+   Roll No: 204G1A0552
+   Title of the Experiment: CPU Scheduling Using PP.C Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
