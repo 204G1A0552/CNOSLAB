c3f8730513dd3d71ae6fcec98a33cae50bd52946
204G1A0552 <94515230+204G1A0552@users.noreply.github.com>
2022-10-11 11:41:28 +0530

    P Manjujsha Created PC.c

---
+++ OSLAB/PC.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: P.Manjusha
+   Roll No: 204G1A0552
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
