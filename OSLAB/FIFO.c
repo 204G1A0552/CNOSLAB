40a1708dc6c260fb08951654dcdbe91f8ee713bd
204G1A0552 <94515230+204G1A0552@users.noreply.github.com>
2022-11-01 12:44:44 +0530

    P.Manjusha Created FIFO.c

---
+++ OSLAB/FIFO.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: P.Manjusha
+   Roll No: 204G1A0552
+   Title of the Experiment: CPU Scheduling Using FIFO Algorithm
+   Date of Creation: 1-11-2022
+   Date of Execution: 1-11-2022
+*/
