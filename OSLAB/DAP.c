1ba8a4f6b1648a14149272d0a5e91c7b42dd1f04
204G1A0552 <204G1A0552@srit.ac.in>
2022-11-10 15:47:48 +0530

    My program is completed

--- OSLAB/DAP.c
+++ OSLAB/DAP.c
@@ -5,7 +5,7 @@
    Date of Execution: 8-11-2022
 */
 #include<stdio.h>
-#include<conio.h>
+//#include<conio.h>
 #include<string.h>
 void main()
 {
@@ -15,7 +15,7 @@ int i,j,k,n,need[10][10];
 int m;
 int count=0,c=0;
 char finish[10];
-clrscr();
+//clrscr();
 printf("Enter the no. of processes and resources:");
 scanf("%d%d",&n,&m);
 for(i=0;i<=n;i++)
@@ -71,5 +71,5 @@ goto A;
 else
 printf("\n System is in safe mode");
 printf("\n The given state is safe state");
-getch();
+//getch();
 }
